ae7ef9e1bde09550e023e22fdc05d81dee1631e7
AqeelAbbasr12 <aqeelabbasr12@gmail.com>
2024-05-23 14:10:58 +0100

    home page design with responsive

---
+++ js/custom.js
@@ -0,0 +1,209 @@
+let items = document.querySelectorAll('#serviceCarousel.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let homeCaringItems = document.querySelectorAll('#caringCarousel.carousel .carousel-item')
+
+homeCaringItems.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = homeCaringItems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let artisticCarouselItems = document.querySelectorAll('#artisticCarousel.carousel .carousel-item')
+
+artisticCarouselItems.forEach((el) => {
+    const minPerSlide = 3
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = artisticCarouselItems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let cookingCarouselItems = document.querySelectorAll('#cookingCarousel.carousel .carousel-item')
+
+cookingCarouselItems.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = cookingCarouselItems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let avialableHomeWorkCarouselitems = document.querySelectorAll('#avialableHomeWorkCarousel.carousel .carousel-item')
+
+avialableHomeWorkCarouselitems.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = avialableHomeWorkCarouselitems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let availableCaringCarouselItems = document.querySelectorAll('#availableCaringCarousel.carousel .carousel-item')
+
+availableCaringCarouselItems.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = availableCaringCarouselItems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let availableArtisticCarouselItems = document.querySelectorAll('#availableArtisticCarousel.carousel .carousel-item')
+
+availableArtisticCarouselItems.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = availableArtisticCarouselItems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let availableCookingCarouselItems = document.querySelectorAll('#availableCookingCarousel.carousel .carousel-item')
+
+availableCookingCarouselItems.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = availableCookingCarouselItems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+// Professional Carousel Script
+let professionalHomeWorkCarouselitems = document.querySelectorAll('#professionalCarousel.carousel .carousel-item')
+
+professionalHomeWorkCarouselitems.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = professionalHomeWorkCarouselitems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let professionalCaringCarouselItems = document.querySelectorAll('#professionalCaringCarousel.carousel .carousel-item')
+
+professionalCaringCarouselItems.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = professionalCaringCarouselItems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let professionalArtisticCarouselItems = document.querySelectorAll('#professionalArtisticCarousel.carousel .carousel-item')
+
+professionalArtisticCarouselItems.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = professionalArtisticCarouselItems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let professionalCookingCarouselItems = document.querySelectorAll('#professionalCookingCarousel.carousel .carousel-item')
+
+professionalCookingCarouselItems.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = professionalCookingCarouselItems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+// Customer Carousel
+let customerCarouselItems = document.querySelectorAll('#customerCarousel.carousel .carousel-item')
+
+customerCarouselItems.forEach((el) => {
+    const minPerSlide = 1
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = customerCarouselItems[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
